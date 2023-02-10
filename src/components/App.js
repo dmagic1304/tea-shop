9bfb8eaf442dd77f41902d4c22c7f4f8a869e549
Dominik Magic <dominikmagic5@gmail.com>
2023-02-10 09:08:34 -0800

    TeaController component added to hold state

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
+import TeaController from "./TeaController";
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <TeaController />
     </div>
   );
 }
---
+++ src/components/TeaController.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class TeaController extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+
+  render() {
+    return(
+      <p>main</p>
+    )
+  }
+
+}
+
+export default TeaController;

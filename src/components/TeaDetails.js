6ca89b144f9e031d461862bcd9456bfe8d78f592
Dominik Magic <dominikmagic5@gmail.com>
2023-02-10 13:34:16 -0800

    details view completed

--- src/components/TeaDetails.js
+++ src/components/TeaDetails.js
@@ -5,13 +5,25 @@ function TeaDetails({tea}){
 
   return (
     <div>
-      <h1>{tea.name}</h1>      
+      <h1>{tea.name}</h1>
+      <p>Origin: {tea.origin}</p>
+      <p>Roast: {tea.roast}</p>
+      <p>Price: ${tea.price}</p>
+      <p>Pounds sold: {tea.poundsSold} lb</p>
+      <p>Pounds Remaining in crate: {tea.poundsRemaining} lb</p>      
     </div>
   );
 }
 
 TeaDetails.propTypes = {
- 
+  name: PropTypes.string,
+  origin: PropTypes.string,
+  roast: PropTypes.string,
+  price: PropTypes.string,
+  poundsRemaining: PropTypes.number,
+  poundsSold: PropTypes.number,
 };
+
+
 
 export default TeaDetails;

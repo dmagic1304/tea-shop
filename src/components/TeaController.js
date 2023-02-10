a592b1ceec4087a21e51bb9a415be2f9b713c57a
Dominik Magic <dominikmagic5@gmail.com>
2023-02-10 09:35:29 -0800

    added default inventory and set it as state

--- src/components/TeaController.js
+++ src/components/TeaController.js
@@ -1,11 +1,12 @@
 import React from "react";
+import inventoryList from "./inventoryList";
 
 class TeaController extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-
+      inventory: inventoryList
     };
   }
 
---
+++ src/components/inventoryList.js
@@ -0,0 +1,40 @@
+let inventoryList = [  
+ {  
+    name: "Camomile",
+    origin: "Asia",
+    roast: "Sed ut perspiciatis unde omnis",
+    price: "$13"
+ },
+ {  
+    name: "Green",
+    origin: "Alaska",
+    roast: "Sed ut perspiciatis unde omnis",
+    price: "$4"
+ },
+ {  
+    name: "Black",
+    origin: "New Zeland",
+    roast: "Sed ut perspiciatis unde omnis",
+    price: "$8"
+ },
+ {  
+    name: "Fruit",
+    origin: "Spain",
+    roast: "Sed ut perspiciatis unde omnis",
+    price: "$6"
+ },
+ {  
+    name: "Herbal",
+    origin: "Chile",
+    roast: "Sed ut perspiciatis unde omnis",
+    price: "$14"
+ },
+ {  
+    name: "Sweet",
+    origin: "Argentina",
+    roast: "Sed ut perspiciatis unde omnis",
+    price: "$8"
+ }
+];
+
+export default inventoryList;

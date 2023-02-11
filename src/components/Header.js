78bd10f553613efeee89e6066d2cd969ce14204a
Dominik Magic <dominikmagic5@gmail.com>
2023-02-10 16:11:54 -0800

    finished TeaList styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
   return (
-    <div class="justify-center flex" >
+    <div class="justify-center flex text-5xl m-6 font-bold" >
       <h1>Tea Shop</h1>
     </div>
   )
--- src/components/Tea.js
+++ src/components/Tea.js
@@ -4,10 +4,10 @@ import PropTypes from "prop-types";
 
 function Tea({name, origin, roast}) {
   return (
-    <div>
-      <h3>{name}</h3>
-      <p>Origin: {origin}</p>
-      <p>Roast: {roast}</p> 
+    <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+      <h3 class = "mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white" >{name}</h3>
+      <p class="font-normal text-gray-700 dark:text-gray-400">Origin: {origin}</p>
+      <p class="font-normal text-gray-700 dark:text-gray-400">Roast: {roast}</p> 
     </div>
   )
 }
